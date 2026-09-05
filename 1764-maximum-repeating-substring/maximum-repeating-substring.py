e096d8269a8368fc6d8860d0904b2bfc74d4cd15
P SAHANA <iamsahanapblr@gmail.com>
2026-09-05 23:44:24 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (53.78%) - LeetSync

---
+++ 1764-maximum-repeating-substring/maximum-repeating-substring.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maxRepeating(self, sequence: str, word: str) -> int:
+        k = 0
+        # Keep checking as long as the repeated word is a substring of sequence
+        while (word * (k + 1)) in sequence:
+            k += 1
+            
+        return k
