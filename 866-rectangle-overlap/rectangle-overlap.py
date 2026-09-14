d99c0eaefa1a4924103231d92193f9cab3ef5b72
P SAHANA <iamsahanapblr@gmail.com>
2026-09-14 19:34:38 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (59.18%) - LeetSync

---
+++ 866-rectangle-overlap/rectangle-overlap.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isRectangleOverlap(self, rec1: List[int], rec2: List[int]) -> bool:
+        # Check if one rectangle is completely to the left, right, above, or below the other
+        return not (rec1[2] <= rec2[0] or  # rec1 is to the left of rec2
+                    rec1[0] >= rec2[2] or  # rec1 is to the right of rec2
+                    rec1[3] <= rec2[1] or  # rec1 is below rec2
+                    rec1[1] >= rec2[3])    # rec1 is above rec2
